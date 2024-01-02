6d13bbad0b76d0cacd987b706d2fa653ef047bba
ize-302 <adavizeozorku@gmail.com>
2024-01-02 10:09:24 +0100

    chore: refactor

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,16 @@
 from passlib.context import CryptContext
+from typing import Union
+from jose import jwt
+from datetime import timedelta, datetime
+from jose import jwt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
+algorithm = os.getenv("ALGORITHM")
+access_token_expire_minutes = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +19,13 @@ def hash_password(password):
   
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+  
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+  to_encode = data.copy()
+  if expires_delta:
+      expire = expires_delta
+  else:
+      expire = datetime.utcnow() + timedelta(minutes=15)
+  to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+  return encoded_jwt
